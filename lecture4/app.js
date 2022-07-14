c42d73b465472d76c12d701f2a77e8b53c7115d1
RAVIKUMARBALIJA <bravikumar123@hotmail.com>
2022-07-14 09:10:50 +0530

    add lecture 4 and 6

---
+++ lecture4/app.js
@@ -0,0 +1,12 @@
+(function(){
+'use strict';
+
+angular.module("myfirstApp",[])
+.controller('myfirstcontroller',function($scope){
+    $scope.name = "ravikumar";
+    $score.say_hello = function(){
+        return "Hello coursera"
+    };
+});
+
+})();
